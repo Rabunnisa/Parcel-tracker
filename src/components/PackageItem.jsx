97278a9f23d8548b3ccc93242924ac30e6f6ac28
Rabunnisa <rabia.zabiulla@gmail.com>
2021-04-04 13:34:10 +0200

    logo added

--- src/components/PackageItem.jsx
+++ src/components/PackageItem.jsx
@@ -10,12 +10,12 @@ return(
 <div className="right">
   
  <div className="item1"  > 
- <p className="subitem1">Package-Number : {data.parcel_id}</p>
+ <p className="subitem1">Package Id : {(data.parcel_id)}</p>
  <p >Sender : {data.sender}</p>
- <p className="subitem2">Order-status : {data.status}</p>
+ <p className="subitem2">Status : {data.status}</p>
  <time className="date" >ETA : {data.eta}</time>
- <p >location_name : {data.location_name} </p>
- <p >Last-Updated : {data.last_updated} </p>
+ <p >Location : {data.location_name} </p>
+ <p >Last Updated : {data.last_updated} </p>
    </div>
     </div>
     </div>)
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,13 +2,15 @@ import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
+    
     <section className="home">
       <h2>Our solutions connect people and possibilities.</h2>
-      <div>Connecting people with goods, services and ideas creates opportunities and improves lives. We believe that a connected world is a better world, and that belief guides everything we do.</div>
-      <Link className="link" to="/packagetrack">
+      <div class="p1">Connecting people with goods, services and ideas creates opportunities and improves lives. We believe that a connected world is a better world, and that belief guides everything we do.</div>
+      {/* <Link className="link" to="/packagetrack">
         Click Here to package
-      </Link>
-      <div>Home</div>
+      </Link> */}
+      
+      
     </section>
   );
 }
